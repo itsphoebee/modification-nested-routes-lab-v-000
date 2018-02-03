d23f80074b9f559913ff670a8e3f3a385c6fc139
Phoebe Lau <phoebelau7@gmail.com>
2018-02-03 04:27:23 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -6,7 +6,8 @@ module ArtistsHelper
   def artist_select(artist,song)
     if artist.nil?
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
-    
+    else
+      artist.name
 end
 
 #if song.artist.nil ?
