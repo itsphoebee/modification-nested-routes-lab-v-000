15b68bf292ac25bb45bd0212511b8eff905cbdc6
Phoebe Lau <phoebelau7@gmail.com>
2018-02-03 04:08:57 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -46,6 +46,10 @@ class SongsController < ApplicationController
   def edit
     if params[:artist_id]
       artist = Artist.find_by(id:params[:artist_id])
+      if artist.nil?
+        redirect_to artists_path, alert: "Author not found."
+      else
+        @song = artist.songs.find_by(id:params[:id])
     @song = Song.find(params[:id])
   end
 
