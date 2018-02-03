8233e2b5033e6c5110df452241b12a4a264c6218
Phoebe Lau <phoebelau7@gmail.com>
2018-02-03 04:27:12 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,4 +3,15 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
+  def artist_select(artist,song)
+    if artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    
+end
+
+#if song.artist.nil ?
+#   <%= f.select :author_id, options_from_collection_for_select(Author.all, :id, :name) %><br>
+# else
+#
+#  <% end %>
 end
